52da19ff0b1034e64ab375df3fff2d9c72268904
Jeffiy <zhangmin6105@qq.com>
2015-12-26 16:21:55 +0800

    add post delete view

--- src/ZBlog/Controllers/PostController.cs
+++ src/ZBlog/Controllers/PostController.cs
@@ -31,6 +31,7 @@ namespace ZBlog.Controllers
             ViewData["StatusMessage"] =
                 message == ManageMessageId.PostNewSuccess ? "Your post has been posted."
                 : message == ManageMessageId.PostEditSuccess ? "Your post has been updated."
+                : message == ManageMessageId.PostDeleteSuccess ? "Your post has been deleted."
                 : message == ManageMessageId.Error ? "An error has occurred."
                 : "";
 
@@ -125,6 +126,8 @@ namespace ZBlog.Controllers
                 await _dbContext.SaveChangesAsync(requestAborted);
                 
                 await UpdatePostTags(model, tags, requestAborted);
+                
+                _logger.LogDebug($"Updated post<{model.Title}>");
 
                 return RedirectToAction(nameof(Index), new { Message = ManageMessageId.PostEditSuccess });
             }
@@ -132,6 +135,58 @@ namespace ZBlog.Controllers
             ViewBag.Catalogs = await _dbContext.Catalogs.OrderByDescending(c => c.PRI).ToListAsync(requestAborted);
 
             return View(model);
+        }
+
+        // GET: /Post/Delete/id
+        [AdminRequired]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            _logger.LogDebug($"Delete post<{id}>");
+
+            if (!id.HasValue)
+            {
+                return HttpNotFound();
+            }
+
+            var post = await _dbContext.Posts.SingleOrDefaultAsync(p => p.Id == id);
+
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewData["Title"] = $"Post Delete<{post.Title}> Confirmation";
+            
+            return View(post);
+        }
+
+        // POST: /Post/Delete/id
+        [HttpPost]
+        [AdminRequired]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id, CancellationToken requestAborted)
+        {
+            var post = await _dbContext.Posts.Include(p => p.PostTags)
+                        .OrderByDescending(p => p.CreateTime)
+                        .SingleOrDefaultAsync(p => p.Id == id, requestAborted);
+
+            _dbContext.Remove(post);
+            foreach (var postTag in post.PostTags)
+            {
+                //when count of the tag' posts less 1, then delete it.
+                var tag = await _dbContext.Tags.Include(t => t.PostTags).SingleOrDefaultAsync(p => p.Id == postTag.TagId, requestAborted);
+                if (tag.PostTags.Count <= 1)
+                {
+                    _dbContext.Remove(tag);
+                }
+                _dbContext.Remove(postTag);
+            }
+            
+            await _dbContext.SaveChangesAsync(requestAborted);
+
+            _logger.LogDebug($"Deleted post<{post.Title}>");
+
+            return RedirectToAction(nameof(Index), new { Message = ManageMessageId.PostDeleteSuccess });
         }
 
         private async Task UpdatePostTags(Post model, string tags, CancellationToken requestAborted)
@@ -172,6 +227,7 @@ namespace ZBlog.Controllers
         {
             PostNewSuccess,
             PostEditSuccess,
+            PostDeleteSuccess,
             Error
         }
 
