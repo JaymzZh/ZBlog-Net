dc672a2ee1fd44edfe1391c269dc124729591d4f
Jeffiy <zhangmin6105@qq.com>
2016-01-17 11:59:15 +0800

    Reacting to hosting rename

--- src/ZBlog/Program.cs
+++ src/ZBlog/Program.cs
@@ -6,13 +6,13 @@ namespace ZBlog
     {
         public static void Main(string[] args)
         {
-            var application = new WebApplicationBuilder()
-                .UseConfiguration(WebApplicationConfiguration.GetDefault(args))
+            var host = new WebHostBuilder()
+                .UseDefaultConfiguration(args)
                 .UseIISPlatformHandlerUrl()
                 .UseStartup<Startup>()
                 .Build();
 
-            application.Run();
+            host.Run();
         }
     }
 }
