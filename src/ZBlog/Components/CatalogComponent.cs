236e789259f40d854123b9f10f09d1c68b48b374
Jeffiy <zhangmin6105@qq.com>
2016-01-26 11:51:20 +0800

    Add catalog list in index page

---
+++ src/ZBlog/Components/CatalogComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZBlog.Models;
+
+namespace ZBlog.Components
+{
+    [ViewComponent(Name = "Catalog")]
+    public class CatalogComponent : ViewComponent
+    {
+        public CatalogComponent(ZBlogDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        private ZBlogDbContext DbContext { get; }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var posts = await DbContext.Catalogs.Include(c => c.Posts).ToListAsync();
+
+            return View(posts);
+        }
+    }
+}
